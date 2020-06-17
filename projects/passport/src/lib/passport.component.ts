f7a4c006d6bf41b7fa937559e4a9a505f93f00cf
Federico González <federico1818@gmail.com>
2020-06-17 01:27:00 -0300

    add passport library

---
+++ projects/passport/src/lib/passport.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-passport',
+  template: `
+    <p>
+      passport works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PassportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/passport/src/lib/passport.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { PassportComponent } from './passport.component';
+
+
+
+@NgModule({
+  declarations: [PassportComponent],
+  imports: [
+  ],
+  exports: [PassportComponent]
+})
+export class PassportModule { }
